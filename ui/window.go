06584c42795fcb87c7153cca4646c0e4b2212de5
Max Netyaga <max.netyaga@gmail.com>
2025-03-25 11:33:05 +0200

    change bg color

--- ui/window.go
+++ ui/window.go
@@ -131,7 +131,7 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 
