8f8587e02a502ceed1bfaffd996939a13a5fe527
Max Netyaga <max.netyaga@gmail.com>
2025-03-25 13:37:09 +0200

    implement firt task item

--- ui/window.go
+++ ui/window.go
@@ -30,6 +30,11 @@ type Visualizer struct {
 
 	sz  size.Event
 	pos image.Rectangle
+
+	windowSize int
+
+	defFigureSize  int
+	defFigureColor color.Color
 }
 
 func (pw *Visualizer) Main() {
@@ -37,6 +42,12 @@ func (pw *Visualizer) Main() {
 	pw.done = make(chan struct{})
 	pw.pos.Max.X = 200
 	pw.pos.Max.Y = 200
+
+	pw.windowSize = 800
+
+	pw.defFigureSize = 400
+	pw.defFigureColor = color.RGBA{255, 240, 0, 255}
+
 	driver.Main(pw.run)
 }
 
@@ -47,8 +58,8 @@ func (pw *Visualizer) Update(t screen.Texture) {
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
 		Title:  pw.Title,
-		Width:  800,
-		Height: 800,
+		Width:  pw.windowSize,
+		Height: pw.windowSize,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -120,7 +131,16 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 	case mouse.Event:
 		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			// Реакція на натискання кнопки миші.
+			if e.Button == 1 {
+				pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src)
+
+				pw.drawFigure(
+					image.Point{int(e.X), int(e.Y)},
+					pw.defFigureSize,
+					pw.defFigureColor,
+				)
+			}
 		}
 
 	case paint.Event:
@@ -138,9 +158,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	// Відображення фігури.
 	fmt.Println(pw.sz.Bounds())
-	pw.drawFigure(image.Point{400, 400}, 400, color.RGBA{255, 240, 0, 255})
+	pw.drawFigure(image.Point{400, 400}, pw.defFigureSize, pw.defFigureColor)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
